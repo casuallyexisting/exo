f5bb32f0208ef2327d1f451aa7c24b96ec3e99bd
Mason Coles <masocoles@gmail.com>
2021-02-12 15:34:06 -0500

    Added config templates

---
+++ config/rxConfig-template.py
@@ -0,0 +1,10 @@
+coreConfig = {
+    'custom_player': '<Name for the user to act as>',
+    'sudoers': [],
+    'operators': ['1457658580'],
+}
+
+telegram = {
+    'telegram-token': '<Telegram bot API Key>',
+    'botupdates': '<Channel ID for bot updates (starts with a "-")>'
+}
